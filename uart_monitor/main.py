6e4233df437a74bc79a639811d968a16cd4ff5a2
reBuddyIT <rayzhev@gmail.com>
2025-01-17 17:02:57 +0300

    changed buffer size

--- uart_monitor/main.py
+++ uart_monitor/main.py
@@ -3,21 +3,26 @@ import matplotlib.pyplot as plt
 
 # Настройка последовательного порта
 ser = serial.Serial('COM3', 115200)
+ADC_BUFFER_SIZE = 64
 
 try:
     while True:
-        data = ser.read(1024 * 2)  # Читаем 1024 * 2 байта
-        if len(data) == 1024 * 2:  # Проверяем, что получены все данные
+        data = ser.read(ADC_BUFFER_SIZE * 2)  # Читаем буфер * 2 байта
+        if len(data) == ADC_BUFFER_SIZE * 2:  # Проверяем, что получены все данные
             adc_values = []
             for i in range(0, len(data), 2):
                 adc_value = data[i] | (data[i+1] << 8)  # Собираем uint16_t из двух байт
                 adc_values.append(adc_value)
 
+            # Продублирование массива
+            duplicated_adc_values = [value for value in adc_values for _ in range(2)]
+
             # Визуализация данных
-            plt.plot(adc_values)
+            plt.plot(duplicated_adc_values)
+
             plt.xlabel('Sample')
-            plt.ylabel('ADC Value')
-            plt.title('ADC Data')
+            plt.ylabel('ADC')
+            plt.title('Data')
             plt.grid(True)
             plt.pause(0.001)
             plt.clf()
